a58c4d7cc6097fb9b0cc783f757ff5278f2f9bb6
mwafak <Mwafaqarbash3@gmail.com>
2019-01-28 11:38:37 +0100

    project1 added

---
+++ projct1/build/precache-manifest.8abefdb53ab5e8f79fa5b03c384ef311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "438403e2ef9d4ac34072",
+    "url": "/static/js/main.438403e2.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "438403e2ef9d4ac34072",
+    "url": "/static/css/main.80851616.chunk.css"
+  },
+  {
+    "revision": "7ba76831c86b952b9f77748ac689b4b2",
+    "url": "/index.html"
+  }
+];
