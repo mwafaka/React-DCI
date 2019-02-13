cd1384bc9d96eb5f1b2974cfc63b23fb0b506713
mwafak <Mwafaqarbash3@gmail.com>
2019-02-13 12:10:47 +0100

    about modifeid

--- quiz/src/components/About.js
+++ quiz/src/components/About.js
@@ -20,9 +20,9 @@ class About extends React.Component {
 
   onSubmit = e => {
     e.preventDefault();
-    if ((this.state.question = "")) {
-      return;
-    }
+    // if (this.state.question === "") {
+    //   return;
+    // }
     const {
       question,
       choice1,
@@ -50,102 +50,114 @@ class About extends React.Component {
     });
     e.target.reset();
   };
-
+  deleteItem = (e, indexToRemove) => {
+    const questions = this.state.questions.filter(
+      (ele, index) => index !== indexToRemove
+    );
+    this.setState({ questions });
+  };
   render() {
     return (
       <React.Fragment>
-        <Form onSubmit={this.onSubmit} className="styling">
-          <header style={headerStyle}>
-            <h1>Add your Questions</h1>
+        <div className="col-10">
+          <Form onSubmit={this.onSubmit} className="styling">
+            <header style={headerStyle}>
+              <header style={headerStyle} />
+            </header>
+            <FormGroup row>
+              <Label sm={2} className="text-secondary">
+                Question
+              </Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.onChange}
+                  type="text"
+                  name="question"
+                  // value={this.state.name}
+                  placeholder="Add your Questions"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label sm={2} className="text-secondary">
+                Choice 1
+              </Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.onChange}
+                  type="text"
+                  name="choice1"
+                  // id="exampleEmail"
+                  placeholder="Add Choice"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label sm={2} className="text-secondary">
+                Choice 2
+              </Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.onChange}
+                  type="text"
+                  name="choice2"
+                  // id="exampleEmail"
+                  placeholder="Add Choice"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label sm={2} className="text-secondary">
+                Choice 3
+              </Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.onChange}
+                  type="text"
+                  name="choice3"
+                  // id="exampleEmail"
+                  placeholder="Add Choice"
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label sm={2} className="text-secondary">
+                Correct Answer
+              </Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.onChange}
+                  type="text"
+                  name="answer"
+                  // id="exampleEmail"
+                  placeholder="Add Answer"
+                />
+              </Col>
+            </FormGroup>
 
-            <header style={headerStyle} />
-          </header>
-          <FormGroup row>
-            <Label sm={2} className="text-secondary">
-              Question
-            </Label>
-            <Col sm={10}>
-              <Input
-                onChange={this.onChange}
-                type="text"
-                name="question"
-                // value={this.state.name}
-                placeholder="Add your Questions"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label sm={2} className="text-secondary">
-              Choice 1
-            </Label>
-            <Col sm={10}>
-              <Input
-                onChange={this.onChange}
-                type="text"
-                name="choice1"
-                // id="exampleEmail"
-                placeholder="Add Choice"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label sm={2} className="text-secondary">
-              Choice 2
-            </Label>
-            <Col sm={10}>
-              <Input
-                onChange={this.onChange}
-                type="text"
-                name="choice2"
-                // id="exampleEmail"
-                placeholder="Add Choice"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label sm={2} className="text-secondary">
-              Choice 3
-            </Label>
-            <Col sm={10}>
-              <Input
-                onChange={this.onChange}
-                type="text"
-                name="choice3"
-                // id="exampleEmail"
-                placeholder="Add Choice"
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label sm={2} className="text-secondary">
-              Correct Answer
-            </Label>
-            <Col sm={10}>
-              <Input
-                onChange={this.onChange}
-                type="text"
-                name="answer"
-                // id="exampleEmail"
-                placeholder="Add Answer"
-              />
-            </Col>
-          </FormGroup>
-
-          <button>Submit</button>
-        </Form>
-        <br />
-
+            <button className="btn btn-secondary">Submit</button>
+          </Form>
+          <br />
+        </div>
         {this.state.questions &&
-          this.state.questions.map(q => {
+          this.state.questions.map((q, index) => {
             return (
-              <div className="container">
-                <ol className="text-white">
-                  <li>question:{q.question}</li>
-                  <li>choice1:{q.choice1}</li>
-                  <li>choice2:{q.choice2}</li>
-                  <li>choice3:{q.choice3}</li>
-                  <li>answer:{q.answer}</li>
-                </ol>
+              <div className="card text-center col-9">
+                <div className="card-header card-info">New Question</div>
+                <div className="card-body">
+                  <h3>question:{q.question}</h3>
+                  <p className="card-text">choice1:{q.choice1}</p>
+                  <p className="card-text"> choice2:{q.choice2}</p>
+                  <p className="card-text">choice3:{q.choice3}</p>
+                  <h5 className="card-text">answer:{q.answer}</h5>
+
+                  <button
+                    className=" btn btn-secondary text-white  btn-sm  "
+                    onClick={e => this.deleteItem(e, index)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             );
           })}
